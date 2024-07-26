b91be630c1875d2604cb7f57cb6660fbc11fd1cd
okata-t <katatsumuri0308@gmail.com>
2024-07-27 03:16:36 +0900

    Add: フォルダ選択
    保存先フォルダをダイアログで指定
    URLをクリップボードからペーストするボタン

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import os
+import tkinter as tk
+from tkinter import filedialog, messagebox
+
 import customtkinter as ctk
-from tkinter import messagebox
+import pyperclip
 
 
 class App(ctk.CTk):
@@ -16,12 +20,46 @@ class App(ctk.CTk):
         self.setup()
 
     def setup(self):
-        ent_url = ctk.CTkEntry(self, width=400, placeholder_text="URL", font=self.fonts)
-        ent_url.grid(row=0, column=0, padx=20, pady=20)
-        btn_download = ctk.CTkButton(
+        self.ent_url = ctk.CTkEntry(
+            self, width=400, placeholder_text="URL", font=self.fonts
+        )
+        self.ent_url.grid(row=0, column=0, padx=20, pady=10)
+
+        self.btn_paste = ctk.CTkButton(
+            self, width=100, text="ペースト", command=self.paste, font=self.fonts
+        )
+        self.btn_paste.grid(row=0, column=1, padx=20, pady=10)
+
+        self.ent_savedir = ctk.CTkEntry(
+            self, width=400, placeholder_text="保存先フォルダ", font=self.fonts
+        )
+        self.ent_savedir.grid(row=1, column=0, padx=20, pady=10)
+
+        self.btn_savedir = ctk.CTkButton(
+            self, width=100, text="参照", command=self.savedir, font=self.fonts
+        )
+        self.btn_savedir.grid(row=1, column=1, padx=20, pady=10)
+
+        self.ent_filename = ctk.CTkEntry(
+            self, placeholder_text="保存ファイル名", width=400, font=self.fonts
+        )
+        self.ent_filename.grid(row=2, column=0, padx=20, pady=10)
+
+        self.btn_download = ctk.CTkButton(
             self, width=100, text="ダウンロード", command=self.download, font=self.fonts
         )
-        btn_download.grid(row=0, column=1, padx=20, pady=20)
+        self.btn_download.grid(row=2, column=1, padx=20, pady=10)
+
+    def paste(self):
+        clip_text = pyperclip.paste()
+        self.ent_url.delete(0, tk.END)
+        self.ent_url.insert(0, clip_text)
+
+    def savedir(self):
+        current_dir = os.path.abspath(os.path.dirname(__file__))
+        file_path = filedialog.askdirectory(initialdir=current_dir)
+        self.ent_savedir.delete(0, tk.END)
+        self.ent_savedir.insert(0, file_path)
 
     def download(self):
         messagebox.showinfo("ダウンロード", "まだ作ってないよ")
